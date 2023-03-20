8aa940b4f2f490cf021c654bb444e9c92e08baef
dc339 <37876058+wangjingyi1@users.noreply.github.com>
2023-03-20 16:28:08 +0800

    test

--- backend/src/test/java/com/weiyan/atp/service/impl/DABEServiceImplTest.java
+++ backend/src/test/java/com/weiyan/atp/service/impl/DABEServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.weiyan.atp.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weiyan.atp.AbeTrustPlatformApplication;
+import com.weiyan.atp.data.request.web.DecryptContentRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import org.junit.runner.RunWith;
@@ -19,11 +22,17 @@ import org.springframework.web.context.WebApplicationContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 @Slf4j
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = AbeTrustPlatformApplication.class)
 class DABEServiceImplTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Autowired
     private WebApplicationContext webApplicationContext;
 
@@ -72,5 +81,30 @@ class DABEServiceImplTest {
 
     @Test
     void verifyABSCert() {
+    }
+
+    @Test
+    void DecryptFile() throws Exception {
+        String uri = "/content/decryption";
+
+        DecryptContentRequest mockRequest = new DecryptContentRequest();
+        mockRequest.setCipher("50f0cb242140e7abbca37f5ff973ab4e");
+        mockRequest.setFileName("深圳市 深圳市福田区 气象 福田区-气象数据.xlsx.xlsx");
+        mockRequest.setSharedUser("深圳市气象局");
+        mockRequest.setTags(Arrays.asList("深圳市", "深圳市福田区", "气象", "福田区-气象数据.xlsx"));
+        mockRequest.setUserName("深圳市");
+
+        MockMvc mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MvcResult result = mvc.perform(
+                MockMvcRequestBuilders.post(uri)
+                        .content(objectMapper.writeValueAsString(mockRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn();
+
+
+        int status = result.getResponse().getStatus();
+//        Assert.assertEquals("错误",200,status);
+
     }
 }
